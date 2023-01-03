5a62b48a0b8a0f59c0c4f5264c780b36ec20a74a
ristofer <cristopher.gomez@ug.uchile.cl>
2023-01-03 18:46:58 -0300

    first commit

---
+++ components/ContainerBlock.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Navbar from "./Navbar";
+
+export default function ContainerBlock({ children, ...customMeta }) {
+    const router = useRouter();
+
+    const meta = {
+        title: "Cristopher Gómez - Dev",
+        description: "Hola me llamo Cristopher",
+        image: "/avatar.png",
+        type: "website",
+        ...customMeta,
+    };
+
+    return (
+        <div>
+            <Head>
+                <title>{meta.title}</title>
+                <meta name="robots" content="follow, index" />
+                <meta content={meta.description} name="description" />
+                <meta
+                    property="og:url"
+                    content={`https://gatito.codes${router.asPath}`}
+                />
+                <link
+                    rel="canonical"
+                    href={`https://gatito.codes${router.asPath}`}
+                />
+                <meta property="og:type" content={meta.type} />
+                <meta property="og:site_name" content={"Cris - Gatito"} />
+                <meta property="og:description" content={meta.description} />
+                <meta property="og:title" content={meta.title} />
+                <meta property="og:image" content={meta.image} />
+                {meta.date && (<meta property="article:published_time" content={meta.date} />
+                )}
+            </Head>
+            <main className="bg-red-600 dark:bg-gray-800 w-full">
+                <Navbar />
+                <div>{children}</div>
+            </main>
+        </div>
+    );
+}
---
+++ components/Hero.js
@@ -0,0 +1,28 @@
+import React from "react";
+import userData from "@constants/data";
+
+export default function Hero() {
+    return (
+        <div className="flex flex-row justify-center lg:justify-between bg-purple-500 dark:bg-purple-800">
+            <div className="hidden lg:flex w-1/3">
+
+            </div>
+            <div className="w-full lg:flex lg:flex-col md:w-1/3 text-center md:text-left mx-auto lg:p-20">
+                <h1 className="text-4xl md:text-8xl font-bold text-gray-300 dark:text-gray-200 my-2">
+                    Fullstack Dev
+                </h1>
+                <h1 className="text-4xl md:text-8xl font-bold text-gray-300 dark:text-gray-200 my-2">
+                    Cristopher
+                </h1>
+                <h1 className="text-4xl md:text-8xl font-bold text-gray-300 dark:text-gray-200 my-2">
+                    Gómez
+                </h1>
+            </div>
+            <div className="hidden lg:p-20 lg:flex w-1/2 md:w-1/3 justify-start center-align">
+                <div className="w-1/2">
+                    <img src={userData.avatarUrl} alt="cris" className="shadow rounded-full" />
+                </div>
+            </div>
+        </div>
+    );
+};
