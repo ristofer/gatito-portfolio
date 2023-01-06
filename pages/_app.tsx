f2979b8c6563335a4174bfbf5a00d20de6a0f4f0
ristofer <cristopher.gomez@ug.uchile.cl>
2023-01-06 11:52:43 -0300

    add analytics of vercel to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
+
   );
 }
